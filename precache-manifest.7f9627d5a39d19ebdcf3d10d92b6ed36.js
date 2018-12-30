6afe5890b4dff0cdc854d73de11ebf3299bf4b10
ShizukuIchi <shizukuichi@gmail.com>
2018-12-31 01:26:15 +0800

    Updates

---
+++ precache-manifest.7f9627d5a39d19ebdcf3d10d92b6ed36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f1aefb0f7ecdf1d62b3635bd0c6882",
+    "url": "/slot/static/media/slot.d2f1aefb.png"
+  },
+  {
+    "revision": "3f32ca1ce195e7de6b341f02b799dfb1",
+    "url": "/slot/static/media/slot-button.3f32ca1c.png"
+  },
+  {
+    "revision": "68522c2fc7a30959a6d0c17256daab99",
+    "url": "/slot/static/media/handle-large.68522c2f.png"
+  },
+  {
+    "revision": "1a6d728a8d7a3a667630",
+    "url": "/slot/static/js/runtime~main.1a6d728a.js"
+  },
+  {
+    "revision": "5f396a9bc4fbf1fa4df9",
+    "url": "/slot/static/js/main.5f396a9b.chunk.js"
+  },
+  {
+    "revision": "f287413cc7409129c39f",
+    "url": "/slot/static/js/1.f287413c.chunk.js"
+  },
+  {
+    "revision": "5f396a9bc4fbf1fa4df9",
+    "url": "/slot/static/css/main.3dbb1699.chunk.css"
+  },
+  {
+    "revision": "bf47b52427d962b2b9a962f7dd84eef1",
+    "url": "/slot/index.html"
+  }
+];
