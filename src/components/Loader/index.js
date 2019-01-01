bab936a897efb8889fde411218cd95d556a88ff6
ShizukuIchi <shizukuichi@gmail.com>
2019-01-01 19:51:12 +0800

    clean initiator after loaded

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import posed from 'react-pose';
+import posed, { PoseGroup } from 'react-pose';
 
 export default function Loader(props) {
   const [loaded, setLoaded] = useState(false);
@@ -8,29 +8,31 @@ export default function Loader(props) {
     setLoaded(true);
   }
   return (
-    <>
-      <PosedDiv
-        className={props.className}
-        style={{ position: 'absolute' }}
-        pose={loaded ? 'exit' : 'enter'}
-      >
-        {props.loader(onLoaded)}
-      </PosedDiv>
-      <PosedDiv className={props.className} pose={loaded ? 'enter' : 'exit'}>
-        <props.component />
-      </PosedDiv>
-    </>
+    <PoseGroup>
+      {!loaded && (
+        <PosedDiv
+          key="loader"
+          className={props.className}
+          style={{ position: 'absolute' }}
+        >
+          {props.loader(onLoaded)}
+        </PosedDiv>
+      )}
+      {loaded && (
+        <PosedDiv key="app" className={props.className}>
+          <props.component />
+        </PosedDiv>
+      )}
+    </PoseGroup>
   );
 }
 
 const PosedDiv = posed.div({
   enter: {
-    zIndex: 1,
     opacity: 1,
     delay: 100,
   },
   exit: {
-    zIndex: 0,
     opacity: 0,
   },
 });
